e68d09be7f2a001df2ffefeedd970a84e5fc61bc
ishpat22 <ishaanpat22@gmail.com>
2023-06-05 11:17:34 -0400

    Switched back to pygame

--- KanBoard_5.py
+++ KanBoard_5.py
@@ -1,4 +1,5 @@
-import tkinter as tk
+import pygame
+import sys
 
 def menu_button_action(label):
     if label == "Whiteboard":
@@ -13,27 +14,61 @@ def menu_button_action(label):
         print("Timer button clicked")
         # Add code for the action of the Timer button
     elif label == "Exit":
-        exit()
+        pygame.quit()
+        sys.exit()
 
 def menu_buttons():
-    root = tk.Tk()
-    root.attributes("-fullscreen", True)
-    root.title("Menu Buttons")
+    pygame.init()
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    clock = pygame.time.Clock()
 
     button_labels = ["Whiteboard", "Kanban Board", "Calendar", "Timer", "Exit"]
+    button_width = 200
+    button_height = 50
+    button_spacing = 20
+    button_font = pygame.font.Font(None, 24)
+    button_color = (0, 0, 0)
+    button_text_color = (255, 255, 255)
 
-    button_frame = tk.Frame(root, bg="white")
-    button_frame.pack(side="bottom", pady=20)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # Left mouse button
+                    mouse_pos = pygame.mouse.get_pos()
+                    for i, label in enumerate(button_labels):
+                        button_rect = pygame.Rect(
+                            (screen.get_width() - button_width) // 2,
+                            (screen.get_height() - (button_height + button_spacing) * len(button_labels)) // 2
+                            + i * (button_height + button_spacing),
+                            button_width,
+                            button_height
+                        )
+                        if button_rect.collidepoint(mouse_pos):
+                            menu_button_action(label)
 
-    for label in button_labels:
-        button = tk.Button(button_frame, text=label, width=20, height=2, fg="white", bg="black", command=lambda l=label: menu_button_action(l))
-        button.pack(side="left", padx=10)
+        screen.fill((255, 255, 255))
 
-    # Add an empty label to center-align the buttons
-    empty_label = tk.Label(button_frame, bg="white")
-    empty_label.pack(side="left", expand=True, fill="x")
+        for i, label in enumerate(button_labels):
+            button_rect = pygame.Rect(
+                (screen.get_width() - button_width) // 2,
+                (screen.get_height() - (button_height + button_spacing) * len(button_labels)) // 2
+                + i * (button_height + button_spacing),
+                button_width,
+                button_height
+            )
 
-    root.mainloop()
+            pygame.draw.rect(screen, button_color, button_rect)
+            pygame.draw.rect(screen, (0, 0, 0), button_rect, 3)
+
+            button_text = button_font.render(label, True, button_text_color)
+            text_rect = button_text.get_rect(center=button_rect.center)
+            screen.blit(button_text, text_rect)
+
+        pygame.display.flip()
+        clock.tick(30)
 
 if __name__ == "__main__":
     menu_buttons()
